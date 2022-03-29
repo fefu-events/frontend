351433d7c9ba7fdc7dace686e688f3d7ed17a773
Maxim <rootki7@protonmail.com>
2022-03-30 07:33:42 +1000

    refactoring Calendar and Toggle components

--- src/components/custom/Calendar.vue
+++ src/components/custom/Calendar.vue
@@ -2,15 +2,51 @@
   <div>
     <DatePicker
       v-model="date"
-      :mode="isRange ? 'date' : 'dateTime'"
+      :mode="mode"
       :modelConfig="modelConfig"
       :min-date="Date.now()"
-      is24hr
       color="orange"
       :masks="masks"
+      is24hr
       :is-range="isRange"
-      :is-dark="isDark ? true : false"
+      :is-dark="isDark"
+      class="!hidden md:!inline-flex my-2"
     />
+    <DatePicker
+      v-model="date"
+      :mode="mode"
+      :modelConfig="modelConfig"
+      :min-date="Date.now()"
+      color="orange"
+      :masks="masks"
+      is24hr
+      :is-range="isRange"
+      :is-dark="isDark"
+    >
+      <template v-slot="{ togglePopover }">
+        <div class="flex w-full mx-auto my-2 items-center md:hidden">
+          <button
+            class="p-2 bg-white border border-gray-200 border-r-0 hover:bg-white text-primary rounded-l focus:bg-primary focus:text-white focus:border-primary focus:outline-none"
+            @click="togglePopover()"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              class="w-4 h-4 fill-current"
+            >
+              <path
+                d="M1 4c0-1.1.9-2 2-2h14a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V4zm2 2v12h14V6H3zm2-6h2v2H5V0zm8 0h2v2h-2V0zM5 9h2v2H5V9zm0 4h2v2H5v-2zm4-4h2v2H9V9zm0 4h2v2H9v-2zm4-4h2v2h-2V9zm0 4h2v2h-2v-2z"
+              ></path>
+            </svg>
+          </button>
+          <input
+            :value="normalizedDate.label"
+            class="bg-white text-gray-700 w-full leading-6 py-1 px-2 appearance-none border rounded-r focus:outline-none focus:border-primary"
+            readonly
+          />
+        </div>
+      </template>
+    </DatePicker>
   </div>
 </template>
 
@@ -41,36 +77,50 @@ export default {
   props: {
     isRange: Boolean,
     isDark: Boolean,
+    mode: String,
   },
   data() {
     return {
-      date: Date.now(),
+      date: null,
       modelConfig: {
         type: "string",
         minuteIncrement: 10,
-        mask: "YYYY-MM-DD HH:mm",
+        mask: "DD.MM.YYYY HH:mm",
       },
       masks: {
         title: "MMMM YYYY",
         weekdays: "WWW",
       },
-      range: {
-        start: null,
-        end: null,
-      },
     };
+  },
+
+  computed: {
+    normalizedDate() {
+      let value_date = this.date;
+      if (this.mode == "date" && this.date)
+        if (this.isRange) {
+          value_date = {
+            start: this.date.start?.split(" ")[0],
+            end: this.date.end?.split(" ")[0],
+          };
+        } else {
+          value_date = this.date?.split(" ")[0];
+        }
+
+      let label_date = value_date?.start
+        ? `${value_date.start} / ${value_date.end}`
+        : value_date;
+
+      return { value: value_date, label: label_date };
+    },
   },
 
   watch: {
     date() {
-      let date_ = null;
-      if (this.isRange) {
-        date_ = {
-          start: this.date.start.split(" ")[0],
-          end: this.date.end.split(" ")[0],
-        };
-      }
-      this.$emit("update", date_ ? date_ : this.date);
+      this.$emit("update", this.normalizedDate.value);
+    },
+    isRange() {
+      this.date = null;
     },
   },
 };
--- src/components/custom/Toggle.vue
+++ src/components/custom/Toggle.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="flex items-center rounded-lg px-4 py-2 cursor-pointer">
     <Switch
-      :class="enable ? 'bg-primary' : 'bg-gray-dark '"
+      v-model="enabled"
+      :class="enabled ? 'bg-primary' : 'bg-gray-dark '"
       class="relative inline-flex flex-shrink-0 h-6 w-12 mr-4 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
     >
       <span
         aria-hidden="true"
-        :class="enable ? 'translate-x-6' : 'translate-x-0'"
+        :class="enabled ? 'translate-x-6' : 'translate-x-0'"
         class="pointer-events-none inline-block w-5 h-5 rounded-full bg-white shadow-lg transform ring-0 transition ease-in-out duration-200"
       />
     </Switch>
@@ -21,8 +22,16 @@ export default {
   components: {
     Switch,
   },
-  props: {
-    enable: Boolean,
+  data() {
+    return {
+      enabled: false,
+    };
+  },
+
+  watch: {
+    enabled() {
+      this.$emit("update", this.enabled);
+    },
   },
 };
 </script>
