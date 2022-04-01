3c1b4b55e8c8f14884d2315e871bbc3da845f697
Maxim <rootki7@protonmail.com>
2022-04-02 03:57:13 +1000

    created event block template

---
+++ src/components/template/EventBlock.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hover:bg-hoverColor cursor-pointer">
+    <div class="w-4/5 md:w-72 mx-auto">
+      <div class="grid grid-flow-row grid-cols-5 gap-3 py-4">
+        <div class="col-span-4 px-4 text-left text-lg">
+          <span> {{ title }} </span>
+        </div>
+        <div
+          class="col-span-1 w-10 h-10 rounded-full overflow-hidden justify-self-end self-center"
+        >
+          <img src="@/assets/img/svg/logo.svg" alt="" class="" />
+        </div>
+        <div class="col-span-5 px-4 text-left">
+          <span>{{ date }}</span>
+        </div>
+      </div>
+      <hr class="border-black" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EventBlockComponent",
+  props: {
+    title: String,
+    icon: String,
+    date: String,
+  },
+};
+</script>
---
+++ src/components/template/index.js
@@ -0,0 +1,3 @@
+export { default as EventBlock } from "./EventBlock.vue";
+export { default as MemberBlock } from "./MemberBlock.vue";
+export { default as OrgBlock } from "./OrgBlock.vue";
