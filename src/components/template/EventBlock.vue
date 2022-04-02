c396fe6f334bb3827c868ff08f4cffe9a986563d
Maxim <rootki7@protonmail.com>
2022-04-03 07:55:08 +1000

    completed Event Block component

--- src/components/template/EventBlock.vue
+++ src/components/template/EventBlock.vue
@@ -1,21 +1,23 @@
 <template>
-  <div class="hover:bg-hoverColor cursor-pointer">
-    <div class="w-4/5 md:w-72 mx-auto">
-      <div class="grid grid-flow-row grid-cols-5 gap-3 py-4">
-        <div class="col-span-4 px-4 text-left text-lg">
-          <span> {{ title }} </span>
-        </div>
-        <div
-          class="col-span-1 w-10 h-10 rounded-full overflow-hidden justify-self-end self-center"
-        >
-          <img src="@/assets/img/svg/logo.svg" alt="" class="" />
-        </div>
-        <div class="col-span-5 px-4 text-left">
-          <span>{{ date }}</span>
-        </div>
+  <div class="w-4/5 md:w-70 mx-auto">
+    <div class="grid grid-flow-row grid-cols-5 gap-3 py-4">
+      <div class="col-span-4 px-4 text-left text-lg">
+        <span> {{ event_data.title }} </span>
       </div>
-      <hr class="border-black" />
+      <div
+        class="flex w-12 rounded-full overflow-hidden justify-self-end self-center"
+      >
+        <img
+          class="self-center justify-self-center"
+          src="@/assets/img/svg/logo.svg"
+          alt="user avatar"
+        />
+      </div>
+      <div class="col-span-5 px-4 text-left">
+        <span>{{ event_data.date }}</span>
+      </div>
     </div>
+    <hr class="border-black" />
   </div>
 </template>
 
@@ -23,9 +25,7 @@
 export default {
   name: "EventBlockComponent",
   props: {
-    title: String,
-    icon: String,
-    date: String,
+    event_data: Object,
   },
 };
 </script>
