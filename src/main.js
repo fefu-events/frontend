c0935b2e475f7724198f6b2962021d6390c08610
Maxim <rootki7@protonmail.com>
2022-04-07 23:47:27 +1000

    removed tiny-emitter

--- src/main.js
+++ src/main.js
@@ -3,12 +3,10 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import MsalAuth from "@/service/auth";
-import Emitter from "tiny-emitter";
 
 import "./assets/css/tailwind.scss";
 
 const app = createApp(App);
 app.config.globalProperties.$msal = new MsalAuth();
-app.config.globalProperties.$emitter = new Emitter();
 
 app.use(store).use(router).mount("#app");
