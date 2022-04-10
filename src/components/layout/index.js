5c3c2aeb7fe752e969c9e06a7132871b9ff92d9f
Maxim <rootki7@protonmail.com>
2022-04-10 21:37:34 +1000

    update index import layouts

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,4 +1,4 @@
 export { default as EventInfoBar } from "./EventInfoBar.vue";
-export { default as FilterBar } from "./FilterBar.vue";
 export { default as EventsListBar } from "./EventsListBar.vue";
-export { default as AuthButton } from "./AuthButton.vue";
+export { default as FilterBar } from "./FilterBar.vue";
+export { default as ProfileInfoBar } from "./ProfileInfoBar.vue";
