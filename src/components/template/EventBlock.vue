b98d870902bfc39b680577224de998db52ff4103
Maxim <rootki7@protonmail.com>
2022-04-15 19:45:47 +1000

    add normalized date with moment.js

--- src/components/template/EventBlock.vue
+++ src/components/template/EventBlock.vue
@@ -12,7 +12,7 @@
         />
       </div>
       <div class="col-span-5 text-left" :class="{ '!col-span-4': edit }">
-        <span>{{ event.date_begin }} - {{ event.date_end }}</span>
+        <span>{{ normalizedDate.start }} - {{ normalizedDate.end }}</span>
       </div>
       <div
         v-if="edit"
@@ -27,6 +27,7 @@
 </template>
 
 <script>
+import moment from "moment";
 import { PencilAltIcon } from "@heroicons/vue/outline";
 
 export default {
@@ -41,6 +42,14 @@ export default {
   },
 
   inject: ["onClickSelectEditEvent"],
+
+  computed: {
+    normalizedDate() {
+      const start = moment(this.event.date_begin).format("DD.MM.YYYY");
+      const end = moment(this.event.date_end).format("DD.MM.YYYY");
+      return { start, end };
+    },
+  },
 
   methods: {
     onClickEdit(eventID) {
