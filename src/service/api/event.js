5e6e15b86a7ca544697cc0369c56be442714cc07
Maxim <rootki7@protonmail.com>
2022-04-15 18:05:54 +1000

    add event api

--- src/service/api/event.js
+++ src/service/api/event.js
@@ -47,7 +47,42 @@ const endpoints = {
       },
     }),
 
-  getByID: (id) => axios.get(`/api/event/${id}`),
+  getAll: (data, user) => {
+    const queryParams = {
+      skip: 0,
+      limit: 10,
+      title: data.query,
+      tags: user.tags ? user.tags.split(" ").filter(Boolean) : [],
+      for_user_id: user.id,
+      subscriptions: data.followToggle,
+      personalize_tags: data.recommendToggle,
+    };
+    if (data.date) {
+      if (data.date.start) {
+        queryParams["date_begin"] = data.date.start;
+      } else {
+        queryParams["date_begin"] = data.date;
+      }
+      if (data.date.end) {
+        queryParams["date_end"] = data.date.end;
+      }
+    }
+
+    axios.get("/api/event", {
+      params: queryParams,
+    });
+  },
+
+  getByUserID: (skip, user_id) =>
+    axios.get("/api/event", {
+      params: {
+        skip: skip,
+        limit: 10,
+        user_id: user_id,
+      },
+    }),
+
+  getByEventID: (id) => axios.get(`/api/event/${id}`),
 };
 
 export default endpoints;
