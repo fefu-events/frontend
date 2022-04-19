716c5e01865a64e04cd16089effee2339becbea5
Maxim <rootki7@protonmail.com>
2022-04-20 08:11:08 +1000

    update create organization layout

--- src/components/layout/CreateOrganizationLayout.vue
+++ src/components/layout/CreateOrganizationLayout.vue
@@ -1,7 +1,5 @@
 <template>
-  <div
-    class="flex flex-col justify-between h-full w-[89%] xl:w-80 mx-auto overflow-y-scroll"
-  >
+  <div class="flex flex-col h-full w-[89%] xl:w-80 mx-auto">
     <!-- Back -->
     <div
       class="absolute top-5 right-5 -scale-x-100 hover:text-primary cursor-pointer"
@@ -11,20 +9,33 @@
     </div>
 
     <!-- Add user list -->
-    <div v-if="addUserList" class="flex flex-col mx-5">
+    <div v-if="addUserList" class="flex flex-col h-full mx-5">
       <div class="mt-10 font-bold cursor-pointer">
         <span>Мои организации</span>
       </div>
-
       <!-- Search input -->
       <Search
         class="mt-4"
         @update="(value) => (userQuery = value)"
         :placeholder="'Поиск'"
       />
+      <div class="mb-4 overflow-y-scroll" ref="users">
+        <div
+          class="hover:bg-hoverColor cursor-pointer"
+          v-for="user in filteredAddUsers"
+          :key="user"
+        >
+          <MemberBlock :user="user" :createMode="true" :addMode="true" />
+          <MemberBlock :user="user" :createMode="true" :addMode="true" />
+          <MemberBlock :user="user" :createMode="true" :addMode="true" />
+          <MemberBlock :user="user" :createMode="true" :addMode="true" />
+          <MemberBlock :user="user" :createMode="true" :addMode="true" />
+        </div>
+      </div>
     </div>
 
-    <div v-else class="flex flex-col my-6">
+    <!-- Create page -->
+    <div v-else class="flex flex-col h-full mt-6 overflow-hidden">
       <!-- Avatar -->
       <div class="flex flex-col mx-5 mt-8">
         <div class="w-24 h-24 mx-auto rounded-full text-center bg-gray-dark" />
@@ -55,7 +66,7 @@
       <div class="relative mt-4 xl:!mt-10 mx-5">
         <textarea
           v-model="organization.description"
-          class="w-full h-24 xl:h-36 px-6 py-2 rounded text-sm resize-none ring-offset-0 focus:ring-0 focus:border-primary"
+          class="w-full h-24 xl:h-32 px-6 py-2 rounded text-sm resize-none ring-offset-0 focus:ring-0 focus:border-primary"
           :maxlength="maxDescSize"
           placeholder="Описание"
         />
@@ -67,7 +78,7 @@
           v-text="maxDescSize - organization.description.length"
         />
       </div>
-      <!-- Members -->
+      <!-- Add members -->
       <div class="mt-7 mx-5 xl:mx-0">
         <div class="flex flex-row items-center justify-between xl:mx-5">
           <span> Участники </span>
@@ -76,11 +87,23 @@
           </Button>
         </div>
       </div>
-    </div>
+      <!-- Members -->
+      <div class="my-5 overflow-y-scroll">
+        <div
+          class="hover:bg-hoverColor cursor-pointer"
+          v-for="member in organization.members"
+          :key="member"
+        >
+          <MemberBlock :user="member" :createMode="true" :removeMode="true" />
+        </div>
+      </div>
 
-    <Button v-if="!addUserList" class="mx-10 mb-10" @click="onClickSubmit">
-      <span> Создать организацию </span>
-    </Button>
+      <div class="flex flex-col mt-auto pb-10">
+        <Button class="mx-10 my-0" v-if="!addUserList" @click="onClickSubmit">
+          <span> Создать организацию </span>
+        </Button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -89,6 +112,7 @@
 import { mapState } from "vuex";
 import { ReplyIcon } from "@heroicons/vue/outline";
 import * as InterfaceComponents from "@/components/interface";
+import { MemberBlock } from "@/components/template";
 import api from "@/service/api";
 
 export default {
@@ -96,7 +120,15 @@ export default {
   components: {
     Button: InterfaceComponents.Button,
     Search: InterfaceComponents.Search,
+    MemberBlock,
     ReplyIcon,
+  },
+
+  provide() {
+    return {
+      addMemberToArray: this.addMemberToArray,
+      removeMemberFromArray: this.removeMemberFromArray,
+    };
   },
 
   inject: ["onClickRightsToggle"],
@@ -104,6 +136,8 @@ export default {
   data() {
     return {
       userQuery: "",
+      page: 1,
+      users: [],
       addUserList: false,
       maxTitleSize: 50,
       maxDescSize: 255,
@@ -120,6 +154,16 @@ export default {
     ...mapState("auth/", {
       token: (state) => state.accessToken,
     }),
+
+    filteredAddUsers() {
+      return this.users;
+      // return this.users.filter(
+      //   (user) =>
+      //     !this.organization.members
+      //       .map((member) => member.id)
+      //       .includes(user.id)
+      // );
+    },
   },
 
   async mounted() {},
@@ -127,15 +171,57 @@ export default {
   methods: {
     backMove() {
       if (this.addUserList) {
+        // remove listener
+        const eventsList = this.$refs.users;
+        eventsList.removeEventListener("scroll", () => this.handleScroll());
+
+        //init states
         this.userQuery = "";
+        this.users = [];
+        this.page = 1;
         this.addUserList = false;
         return;
       }
       this.onClickRightsToggle("createOrg");
     },
 
+    async handleScroll() {
+      const eventsList = this.$refs.users;
+      const scrolling = eventsList.scrollTop + eventsList.clientHeight;
+      if (
+        scrolling >= eventsList.scrollHeight &&
+        this.events.length >= this.page * 10
+      ) {
+        const { data } = await api.event.getAll(
+          this.skip,
+          this.filterParams,
+          this.user
+        );
+        this.events = this.events.concat(data);
+        this.page++;
+      }
+    },
+
     openAddUserList() {
+      const eventsList = this.$refs.users;
+      if (eventsList)
+        eventsList.addEventListener("scroll", () => this.handleScroll());
+      this.loadUsersList();
       this.addUserList = true;
+    },
+
+    async loadUsersList() {
+      this.users = await api.user.getAll().then(({ data }) => data);
+    },
+
+    async addMemberToArray(user) {
+      this.organization.members.push(user);
+    },
+
+    async removeMemberFromArray(user) {
+      this.organization.members = this.organization.members.filter(
+        (member) => member.id !== user.id
+      );
     },
 
     async onClickSubmit() {
@@ -154,7 +240,5 @@ export default {
       }
     },
   },
-
-  watch: {},
 };
 </script>
