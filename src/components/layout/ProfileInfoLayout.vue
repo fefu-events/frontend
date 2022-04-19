77f1b455acd884aff95107e3af0e9eef669fd9a9
Maxim <rootki7@protonmail.com>
2022-04-20 08:12:29 +1000

    stye refactor Profile info layout

--- src/components/layout/ProfileInfoLayout.vue
+++ src/components/layout/ProfileInfoLayout.vue
@@ -1,62 +1,52 @@
 <template>
-  <div
-    class="flex flex-col justify-between h-full w-[89%] xl:w-80 mx-auto overflow-y-scroll"
-  >
-    <div class="flex flex-col">
-      <!-- Person -->
-      <div class="flex flex-row xl:justify-between items-center mt-10 mx-5">
-        <div class="w-16 h-16 rounded-full bg-gray-300">
-          <img src="" alt="" srcset="" />
-        </div>
-        <div class="mx-4 md:mx-6 xl:mr-8 xl:ml-0">
-          <span class="block text-2xl">{{ user?.name.split(" ")[0] }}</span>
-          <span class="block text-2xl">{{ user?.name.split(" ")[1] }}</span>
-        </div>
+  <div class="flex flex-col h-full w-[89%] xl:w-80 mx-auto overflow-hidden">
+    <!-- Person -->
+    <div class="flex flex-row xl:justify-between items-center mt-10 mx-5">
+      <div class="w-16 h-16 rounded-full bg-gray-300">
+        <img src="" alt="" srcset="" />
       </div>
-      <!-- Bookmarks -->
-      <div class="mx-5 mt-8">
-        <ul>
-          <li class="flex flex-row my-5 font-bold cursor-pointer group">
-            <HashtagIcon class="w-5 h-5 mx-2" />
-            <span class="group-hover:text-primary">Мои теги</span>
-          </li>
-          <li class="flex flex-row my-5 font-bold cursor-pointer group">
-            <UserIcon class="w-5 h-5 mx-2" />
-            <span class="group-hover:text-primary"> Мои подписки </span>
-          </li>
-          <li
-            class="flex flex-row my-5 font-bold cursor-pointer group"
-            @click="openOrganizationsList"
-          >
-            <UserGroupIcon class="w-5 h-5 mx-2" />
-            <span class="group-hover:text-primary">Мои организации</span>
-          </li>
-        </ul>
+      <div class="mx-4 md:mx-6 xl:mr-8 xl:ml-0">
+        <span class="block text-2xl">{{ user?.name.split(" ")[0] }}</span>
+        <span class="block text-2xl">{{ user?.name.split(" ")[1] }}</span>
       </div>
-      <!-- Events -->
-      <div>
-        <span class="mx-5 font-medium">Мои мероприятия: </span>
-        <div
-          v-if="events.length > 0"
-          class="h-[40vh] my-1 overflow-y-scroll"
-          ref="events"
+    </div>
+    <!-- Bookmarks -->
+    <div class="mx-5 mt-8">
+      <ul>
+        <li class="flex flex-row my-5 font-bold cursor-pointer group">
+          <HashtagIcon class="w-5 h-5 mx-2" />
+          <span class="group-hover:text-primary">Мои теги</span>
+        </li>
+        <li class="flex flex-row my-5 font-bold cursor-pointer group">
+          <UserIcon class="w-5 h-5 mx-2" />
+          <span class="group-hover:text-primary"> Мои подписки </span>
+        </li>
+        <li
+          class="flex flex-row my-5 font-bold cursor-pointer group"
+          @click="openOrganizationsList"
         >
-          <div
-            class="hover:bg-hoverColor px-5 cursor-pointer"
-            v-for="event in events"
-            :key="event.id"
-          >
-            <EventBlock :event="event" :edit="true" />
-          </div>
-        </div>
-        <div v-else class="my-4">
-          <img
-            class="w-3/5 h-3/5 mx-auto"
-            src="@/assets/img/svg/emptyList.svg"
-            alt=""
-          />
-        </div>
+          <UserGroupIcon class="w-5 h-5 mx-2" />
+          <span class="group-hover:text-primary">Мои организации</span>
+        </li>
+      </ul>
+    </div>
+    <!-- Events -->
+    <span class="mx-5 font-medium">Мои мероприятия: </span>
+    <div v-if="events.length > 0" class="my-1 overflow-y-scroll" ref="events">
+      <div
+        class="hover:bg-hoverColor px-5 cursor-pointer"
+        v-for="event in events"
+        :key="event.id"
+      >
+        <EventBlock :event="event" :edit="true" />
       </div>
+    </div>
+    <div v-else class="my-4">
+      <img
+        class="w-3/5 h-3/5 mx-auto"
+        src="@/assets/img/svg/emptyList.svg"
+        alt=""
+      />
     </div>
     <Button class="mx-10 mb-10" @click="signOut">
       <span> Выйти </span>
