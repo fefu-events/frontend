fd3603ed922700e753dfad76bdb23d9712d10423
Maxim <rootki7@protonmail.com>
2022-04-22 03:51:52 +1000

    add unwrapInjectedRef config

--- src/main.js
+++ src/main.js
@@ -8,5 +8,6 @@ import "./assets/css/tailwind.scss";
 
 const app = createApp(App);
 app.config.globalProperties.$msal = new MsalAuth();
+app.config.unwrapInjectedRef = true;
 
 app.use(store).use(router).mount("#app");
