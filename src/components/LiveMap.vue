1f5b67ade0ba032aed769c2f232a49bb67fade79
Maxim <rootki7@protonmail.com>
2022-04-22 18:58:14 +1000

    FIRST UPDATE MAIN PART OF PROJECT!!! LIVE MAP!!!

--- src/components/LiveMap.vue
+++ src/components/LiveMap.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <l-map
+    <LMap
       :bounds="bounds"
       :maxBounds="maxBounds"
       :zoom="zoom"
@@ -11,50 +11,64 @@
       :center="center"
       @zoom="log_move('move')"
     >
-      <l-tile-layer :url="url" :attribution="attribution" />
-      <l-polygon
+      <LTileLayer :url="url" :attribution="attribution" />
+      <LPolygon
         :lat-lngs="[poly_campus]"
         color="#41b782"
         :fill="true"
         :fillOpacity="0.0"
         fillColor="#41b782"
       />
-      <l-marker
-        :lat-lng="[43.027503, 131.889759]"
+      <LMarker
+        v-for="place in mapInfo"
+        :key="place.id"
+        :lat-lng="[place.latitude, place.longitude]"
+        @click="onClickSelectPlace(place.id)"
         @moveend="log_move('moveend')"
       >
-        <l-icon
+        <LIcon
           :icon-anchor="staticAnchor"
           :popup-anchor="popupAnchor"
           :icon-size="iconSize"
           :icon-url="iconUrl"
         />
-        <l-tooltip>{{ burgers.length }} events</l-tooltip>
-        <l-popup>
-          <div class="overflow-y-scroll max-h-80">
-            <div
-              v-for="b in burgers"
-              :key="b"
-              class="grid grid-cols-2 bg-blue-200 rounded-lg w-60 h-auto mx-4 overflow-hidden"
-              @click="counter += 10"
+        <LTooltip>{{ place.event_count }} events</LTooltip>
+        <LPopup class="w-70">
+          <div v-if="!isLoaded" class="flex items-center h-full py-5">
+            <svg
+              class="animate-spin h-12 w-12 text-primary mx-auto"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
             >
-              <div class="col-span-2 justify-self-center">
-                <img :src="b.img" class="w-full" />
-              </div>
-              <span class="col-span-1 text-center py-4">{{ b.title }}</span>
-              <span class="col-span-1 text-center py-4">{{ b.price }} ₽</span>
-              <!-- Пропагинация -->
-              <button
-                class="col-span-2 p-2 m-2 justify-self-end border-2 rounded-md border-green-400 cursor-pointer hover:bg-green-400 hover:border-opacity-0 active:bg-violet-500 hover:text-white"
-                @click.stop="counter -= 20"
-              >
-                В корзину
-              </button>
+              <circle
+                class="opacity-40"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                stroke-width="2"
+              ></circle>
+              <path
+                class="opacity-80"
+                stroke="currentColor"
+                stroke-width="2"
+                d="M4.85857 5C3.09032 6.80375 2 9.27455 2 12C2 17.5228 6.47715 22 12 22"
+              />
+            </svg>
+          </div>
+          <div v-else class="max-h-80 overflow-y-scroll">
+            <div
+              v-for="event in selectedPlaceEvents"
+              :key="event"
+              class="hover:bg-hoverColor cursor-pointer"
+            >
+              <EventBlock class="w-4/5 xl:px-4" :event="event" />
             </div>
           </div>
-        </l-popup>
-      </l-marker>
-    </l-map>
+        </LPopup>
+      </LMarker>
+    </LMap>
   </div>
 </template>
 
@@ -65,6 +79,8 @@
 </style>
 
 <script>
+import { mapState } from "vuex";
+import { EventBlock } from "@/components/template";
 import {
   LMap,
   LTileLayer,
@@ -77,10 +93,12 @@ import {
 import "leaflet/dist/leaflet.css";
 import MapIcon from "@/assets/img/svg/logo.svg";
 import geojson from "../assets/json/geojson";
+import api from "@/service/api";
 
 export default {
   name: "LiveMapComponent",
   components: {
+    EventBlock,
     LMap,
     LTileLayer,
     LPolygon,
@@ -92,36 +110,17 @@ export default {
 
   data() {
     return {
-      filter: "",
-      counter: 0,
-      testObj: { title: "Title", price: 300 },
-      burgers: [
-        {
-          title: "Big Tasty",
-          price: 490,
-          img: "https://macdonald-s.ru/wp-content/uploads/2021/02/makdonalds-big-tejsti.jpg",
-        },
-        {
-          title: "Big Mac",
-          price: 100,
-          img: "https://macdonald-s.ru/wp-content/uploads/2021/02/makdonalds-big-tejsti.jpg",
-        },
-        {
-          title: "Chiken Burger",
-          price: 250,
-          img: "https://macdonald-s.ru/wp-content/uploads/2021/02/makdonalds-big-tejsti.jpg",
-        },
-        {
-          title: "Royal Mac",
-          price: 290,
-          img: "https://macdonald-s.ru/wp-content/uploads/2021/02/makdonalds-big-tejsti.jpg",
-        },
-        {
-          title: "Happy Meal",
-          price: 690,
-          img: "https://macdonald-s.ru/wp-content/uploads/2021/02/makdonalds-big-tejsti.jpg",
-        },
-      ],
+      mapInterval: null,
+      mapTimeout: 30,
+      mapInfo: null,
+
+      isLoaded: false,
+      page: 1,
+      selectedPlace: null,
+      selectedPlaceEvents: [],
+
+      poly_campus: geojson.FEFU_POLY,
+
       url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
       attribution:
         '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
@@ -137,25 +136,83 @@ export default {
         [43.050454, 131.855313],
         [42.998232, 131.935496],
       ],
+
       popupAnchor: [0, -28],
       staticAnchor: [28, 56],
       iconWidth: 56,
       iconHeight: 56,
-      poly_campus: geojson.FEFU_POLY,
     };
   },
+
   computed: {
+    ...mapState("filter", {
+      filterParams: (state) => state,
+    }),
+
+    ...mapState("auth", {
+      userID: (state) => state.user?.id,
+    }),
+
+    skip() {
+      return 10 * this.page;
+    },
+
     iconUrl() {
       return MapIcon;
     },
+
     iconSize() {
       return [this.iconWidth, this.iconHeight];
     },
   },
+
+  mounted() {
+    this.updateEventList();
+    this.mapInterval = setInterval(() => {
+      this.updateEventList();
+    }, this.mapTimeout * 1000);
+  },
+
   methods: {
+    async updateEventList() {
+      this.mapInfo = await api.map
+        .getInfo(this.filterParams, this.userID)
+        .then(({ data }) => data);
+    },
+
+    async onClickSelectPlace(placeID) {
+      this.isLoaded = false;
+
+      this.selectedPlace = placeID;
+      this.selectedPlaceEvents = await api.map
+        .getByPlaceID(0, this.filterParams, this.selectedPlace, this.user)
+        .then(({ data }) => {
+          this.isLoaded = true;
+          return data;
+        });
+    },
+
     log_move(move) {
       return move;
     },
+  },
+
+  watch: {
+    selectedPlace() {
+      this.page = 1;
+    },
+
+    filterParams: {
+      handler() {
+        this.page = 1;
+        this.updateEventList();
+      },
+      deep: true,
+    },
+  },
+
+  beforeUnmount() {
+    clearInterval(this.mapInterval);
   },
 };
 </script>
