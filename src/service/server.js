fd1cf5e60d7b5d6171087a08d4d052bfcc2dbc7a
JollyBanny <rootki7@protonmail.com>
2022-05-27 16:58:36 +1000

    some edits

--- src/service/server.js
+++ src/service/server.js
@@ -9,16 +9,13 @@ app.use(serveStatic(path.join(__dirname, "../../", "dist")));
 
 app.use("/api", proxy("https://fefu-events-app.herokuapp.com/"));
 
-const port = 8080;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
-  var lines = process.stdout.getWindowSize()[1];
+  console.clear();
   console.log(
     chalk.bgHex("#6bbe7c").black(" DONE ") +
       chalk.hex("#6bbe7c")(" Server started successfully")
   );
   console.log("\n  App running at:");
   console.log("  - Local: " + chalk.hex("#73b4d8")("http://localhost:" + port));
-  for (var i = 0; i < lines - 5; i++) {
-    console.log("");
-  }
 });
